ca1346ff4d96afcb9cd4963a43234d4fccfc78c2
Prophet Lamb <prophet.lamb@gmail.com>
2023-09-24 19:05:12 +0200

    Update example

--- examples/RedditDotnetScraper/Program.cs
+++ examples/RedditDotnetScraper/Program.cs
@@ -2,28 +2,36 @@ using ScrapeAAS;
 using RedditDotnetScraper;
 using Dawn;
 using AngleSharp.Dom;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Concurrent;
 
 var builder = Host.CreateApplicationBuilder(args);
 builder.Services
     .AddScrapeAAS()
     .AddHostedService<TopLevelRedditPostSpider>()
-    .AddDataFlowHandler<RedditTopLevelPostBrief, RedditTopLevelPostCommentsSpider>();
+    .AddDataFlow<RedditPostCommentsSpider>()
+    .AddHostedService<RedditPostRelationalAggegator>().AddDataFlow<RedditPostRelationalAggegator>(true);
 var app = builder.Build();
 app.Run();
 
+// Scraped data models
 sealed record RedditUserId(string Id);
+sealed record RedditTopLevelPost(Url PostUrl, string Title, long Upvotes, long Comments, Url CommentsUrl, DateTimeOffset PostedAt, RedditUserId PostedBy);
+sealed record RedditPostComment(Url PostUrl, Url? ParentCommentUrl, Url CommentUrl, string HtmlText, DateTimeOffset PostedAt, RedditUserId PostedBy);
+// EF Core relational models
+sealed record RedditCommentTree(RedditPostComment Comment, List<RedditCommentTree> Children);
+sealed record RedditPost(RedditTopLevelPost TopLevel, List<RedditCommentTree> Comments);
 
-sealed record RedditTopLevelPost(RedditTopLevelPostBrief Brief, RedditComment Comment);
-sealed record RedditTopLevelPostBrief(Url PostUrl, string Title, long Upvotes, long Comments, Url CommentsUrl, DateTimeOffset PostedAt, RedditUserId PostedBy);
-sealed record RedditComment(Url ParentCommentUrl, Url CommentUrl, string HtmlText, DateTimeOffset PostedAt, RedditUserId PostedBy);
-
+/// <summary>
+/// Scrapes the top level posts from the /r/dotnet subreddit.
+/// </summary>
 sealed class TopLevelRedditPostSpider : BackgroundService
 {
     private readonly IAngleSharpBrowserPageLoader _browserPageLoader;
     private readonly ILogger<TopLevelRedditPostSpider> _logger;
-    private readonly IDataflowPublisher<RedditTopLevelPostBrief> _publisher;
+    private readonly IDataflowPublisher<RedditTopLevelPost> _publisher;
 
-    public TopLevelRedditPostSpider(IAngleSharpBrowserPageLoader browserPageLoader, ILogger<TopLevelRedditPostSpider> logger, IDataflowPublisher<RedditTopLevelPostBrief> publisher)
+    public TopLevelRedditPostSpider(IAngleSharpBrowserPageLoader browserPageLoader, ILogger<TopLevelRedditPostSpider> logger, IDataflowPublisher<RedditTopLevelPost> publisher)
     {
         _browserPageLoader = browserPageLoader;
         _logger = logger;
@@ -37,6 +45,7 @@ sealed class TopLevelRedditPostSpider : BackgroundService
         var document = await _browserPageLoader.LoadAsync(source, stoppingToken);
         var queriedContent = document
             .QuerySelectorAll("div.thing")
+            .AsParallel()
             .Select(div => new
             {
                 PostUrl = div.QuerySelector("a.title")?.GetAttribute("href"),
@@ -47,7 +56,7 @@ sealed class TopLevelRedditPostSpider : BackgroundService
                 PostedAt = div.QuerySelector("time")?.GetAttribute("datetime"),
                 PostedBy = div.QuerySelector("a.author")?.TextContent,
             })
-            .Select(queried => new RedditTopLevelPostBrief(
+            .Select(queried => new RedditTopLevelPost(
                 new(root, Guard.Argument(queried.PostUrl).NotEmpty()),
                 Guard.Argument(queried.Title).NotEmpty(),
                 long.Parse(queried.Upvotes.AsSpan()),
@@ -63,24 +72,28 @@ sealed class TopLevelRedditPostSpider : BackgroundService
     }
 }
 
-sealed class RedditTopLevelPostCommentsSpider : IDataflowHandler<RedditTopLevelPostBrief>
+/// <summary>
+/// Scrapes the comments from a top level post, and related them to their parent comments.
+/// </summary>
+sealed class RedditPostCommentsSpider : IDataflowHandler<RedditTopLevelPost>
 {
     private readonly IAngleSharpBrowserPageLoader _browserPageLoader;
-    private readonly ILogger<RedditTopLevelPostCommentsSpider> _logger;
-    private readonly IDataflowPublisher<RedditComment> _publisher;
+    private readonly ILogger<RedditPostCommentsSpider> _logger;
+    private readonly IDataflowPublisher<RedditPostComment> _publisher;
 
-    public RedditTopLevelPostCommentsSpider(IAngleSharpBrowserPageLoader browserPageLoader, ILogger<RedditTopLevelPostCommentsSpider> logger, IDataflowPublisher<RedditComment> publisher)
+    public RedditPostCommentsSpider(IAngleSharpBrowserPageLoader browserPageLoader, ILogger<RedditPostCommentsSpider> logger, IDataflowPublisher<RedditPostComment> publisher)
     {
         _browserPageLoader = browserPageLoader;
         _logger = logger;
         _publisher = publisher;
     }
 
-    public async ValueTask HandleAsync(RedditTopLevelPostBrief message, CancellationToken cancellationToken = default)
+    public async ValueTask HandleAsync(RedditTopLevelPost message, CancellationToken cancellationToken = default)
     {
         var document = await _browserPageLoader.LoadAsync(message.CommentsUrl, cancellationToken);
         var queriedContent = document
             .QuerySelectorAll("div.commentarea > div.sitetable.nestedlisting div.comment > div.entry")
+            .AsParallel()
             .Select(div => new
             {
                 ParentCommentUrl = div.ParentElement?.ParentElement?.ParentElement is { } childContainer &&
@@ -88,14 +101,15 @@ sealed class RedditTopLevelPostCommentsSpider : IDataflowHandler<RedditTopLevelP
                     childContainer.ParentElement is { } parentComment &&
                     parentComment.ClassList.Contains("comment")
                         ? parentComment.QuerySelector("div.flat-list.buttons > a.bylink")?.GetAttribute("href")
-                        : message.CommentsUrl.ToString(),
+                        : null,
                 CommentUrl = div.QuerySelector("div.flat-list.buttons > a.bylink")?.GetAttribute("href"),
                 HtmlText = div.QuerySelector("div.md")?.InnerHtml,
                 PostedAt = div.QuerySelector("time")?.GetAttribute("datetime"),
                 PostedBy = div.QuerySelector("a.author")?.TextContent,
             })
-            .Select(queried => new RedditComment(
-                new(queried.ParentCommentUrl),
+            .Select(queried => new RedditPostComment(
+                new(message.PostUrl),
+                queried.ParentCommentUrl is null ? new(queried.ParentCommentUrl) : null,
                 new(queried.CommentUrl),
                 Guard.Argument(queried.HtmlText).NotEmpty(),
                 DateTimeOffset.Parse(queried.PostedAt.AsSpan()),
@@ -106,4 +120,144 @@ sealed class RedditTopLevelPostCommentsSpider : IDataflowHandler<RedditTopLevelP
             await _publisher.PublishAsync(comment, cancellationToken);
         }
     }
+}
+
+/// <summary>
+/// Inverts the relational structure of the scraped data from child n->1 parent to parent 1->n children.
+/// </summary>
+sealed class RedditPostRelationalAggegator : BackgroundService, IDataflowHandler<RedditTopLevelPost>, IDataflowHandler<RedditPostComment>
+{
+    private readonly IDataflowPublisher<RedditPost> _publisher;
+    private readonly ILogger<RedditPostRelationalAggegator> _logger;
+    private readonly ConcurrentDictionary<Url, (RedditPost Post, long InsertCommentMissCounter)> _postWithCounterByUrl = new();
+    private readonly ConcurrentBag<RedditPostComment> _headlessComments = new();
+
+    public RedditPostRelationalAggegator(IDataflowPublisher<RedditPost> publisher, ILogger<RedditPostRelationalAggegator> logger)
+    {
+        _publisher = publisher;
+        _logger = logger;
+    }
+
+    public ValueTask HandleAsync(RedditTopLevelPost message, CancellationToken cancellationToken = default)
+    {
+        _postWithCounterByUrl.TryAdd(message.CommentsUrl, (new(message, new()), 0));
+        return default;
+    }
+
+    public ValueTask HandleAsync(RedditPostComment message, CancellationToken cancellationToken = default)
+    {
+        _headlessComments.Add(message);
+        return default;
+    }
+
+    protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+    {
+        while (!stoppingToken.IsCancellationRequested)
+        {
+            InsertHeadlessComments();
+            await PublishInactivePosts(stoppingToken);
+            await Task.Delay(TimeSpan.FromSeconds(1), stoppingToken);
+        }
+    }
+
+
+    private async Task PublishInactivePosts(CancellationToken stoppingToken)
+    {
+        foreach (var (post, commentMissCounter) in _postWithCounterByUrl.Values)
+        {
+            if (commentMissCounter > 100)
+            {
+                await PublishPost(post, stoppingToken).ConfigureAwait(false);
+            }
+        }
+
+        async Task PublishPost(RedditPost post, CancellationToken stoppingToken)
+        {
+            if (_postWithCounterByUrl.TryRemove(post.TopLevel.CommentsUrl, out var postWithCounter))
+            {
+                _logger.LogWarning("Post {RedditTopLevelPost} has {CommentMissCounter} misses, publishing", post.TopLevel, postWithCounter.InsertCommentMissCounter);
+                await _publisher.PublishAsync(postWithCounter.Post, stoppingToken);
+            }
+        }
+    }
+
+    private void InsertHeadlessComments()
+    {
+        // drain the headless comments, and attempt to insert them into the post
+        List<RedditPostComment> failedComments = new();
+        while (_headlessComments.TryTake(out var headlessComment))
+        {
+            if (!_postWithCounterByUrl.TryGetValue(headlessComment.PostUrl, out var postWithCounter))
+            {
+                failedComments.Add(headlessComment);
+                continue;
+            }
+            if (!InsertComment(postWithCounter.Post, headlessComment))
+            {
+                IncrementInsertCommentMissCounter(headlessComment, postWithCounter);
+                failedComments.Add(headlessComment);
+            }
+            else
+            {
+                ResetInsertCommentMissCounter(headlessComment, postWithCounter);
+            }
+        }
+
+        // requeue the failed comments
+        foreach (var comment in failedComments)
+        {
+            _headlessComments.Add(comment);
+        }
+
+        void IncrementInsertCommentMissCounter(RedditPostComment headlessComment, (RedditPost Post, long InsertCommentMissCounter) postWithCounter)
+        {
+            _postWithCounterByUrl.AddOrUpdate(headlessComment.PostUrl, (postWithCounter.Post, postWithCounter.InsertCommentMissCounter + 1), (_, postWithCounter) => (postWithCounter.Post, postWithCounter.InsertCommentMissCounter + 1));
+        }
+
+        void ResetInsertCommentMissCounter(RedditPostComment headlessComment, (RedditPost Post, long InsertCommentMissCounter) postWithCounter)
+        {
+            _postWithCounterByUrl.AddOrUpdate(headlessComment.PostUrl, (postWithCounter.Post, 0), (_, postWithCounter) => (postWithCounter.Post, 0));
+        }
+    }
+
+    private bool InsertComment(RedditPost post, RedditPostComment insertComment)
+    {
+        // insertComments are linked by child.ParentCommentUrl == parent.CommentUrl
+        // insertComments without a parent are top level comments
+        if (insertComment.ParentCommentUrl is null)
+        {
+            post.Comments.Add(new(insertComment, new()));
+            return true;
+        }
+        else
+        {
+            // find the parent comment
+            var parentComment = post.Comments
+                .SelectMany(commentTree => commentTree.Children)
+                .FirstOrDefault(commentTree => commentTree.Comment.CommentUrl == insertComment.ParentCommentUrl);
+            if (parentComment is { })
+            {
+                parentComment.Children.Add(new(insertComment, new()));
+                return true;
+            }
+        }
+        return false;
+    }
+}
+
+sealed class RedditPostSqliteSink
+{
+
+
+}
+
+sealed class RedditPostSqliteContext : DbContext
+{
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseSqlite("Data Source=reddit_dotnet.db");
+    }
+
+    public DbSet<RedditTopLevelPost> TopLevelPosts { get; set; } = default!;
+    public DbSet<RedditPostComment> Comments { get; set; } = default!;
 }
