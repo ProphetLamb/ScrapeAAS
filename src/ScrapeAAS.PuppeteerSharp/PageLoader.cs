410f3490222babad7603ec1ce53f7db7122581a2
Prophet Lamb <prophet.lamb@gmail.com>
2023-10-08 22:04:19 +0200

    Add default request policy

--- src/ScrapeAAS.PuppeteerSharp/PageLoader.cs
+++ src/ScrapeAAS.PuppeteerSharp/PageLoader.cs
@@ -4,15 +4,28 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Http;
 using Microsoft.Extensions.Options;
 using Polly;
+using Polly.Contrib.WaitAndRetry;
+using Polly.RateLimit;
 using PuppeteerSharp;
 
 namespace ScrapeAAS;
 
 public sealed class PageLoaderOptions : IOptions<PageLoaderOptions>
 {
-    public AsyncPolicy? RequestPolicy { get; set; }
+    public AsyncPolicy? RequestPolicy { get; set; } = CreateDefaultRequestPolicy();
 
     PageLoaderOptions IOptions<PageLoaderOptions>.Value => this;
+
+    static AsyncPolicy CreateDefaultRequestPolicy()
+    {
+        var retryWithBackoff = Policy.Handle<Exception>(ex => ex is PuppeteerException or HttpRequestException or RateLimitRejectedException)
+            .WaitAndRetryAsync(Backoff.DecorrelatedJitterBackoffV2(TimeSpan.FromMinutes(2), 10))
+            .WrapAsync(Policy.RateLimitAsync(20, TimeSpan.FromMinutes(1)));
+        var rateLimiter = Policy.Handle<RateLimitRejectedException>()
+            .WaitAndRetryForeverAsync(_ => TimeSpan.FromSeconds(5))
+            .WrapAsync(Policy.RateLimitAsync(1, TimeSpan.FromSeconds(4)));
+        return retryWithBackoff.WrapAsync(rateLimiter);
+    }
 }
 
 public static class PuppeteerBrowserExtensions
